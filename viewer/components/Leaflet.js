88d97257e26aa16ebb313f2a29038a9dca4135ab
Eugene Cheipesh <echeipesh@gmail.com>
2016-04-22 14:46:51 -0400

    fix: pass naked geometry, not feature

--- viewer/components/Leaflet.js
+++ viewer/components/Leaflet.js
@@ -9,16 +9,15 @@ var Leaflet = React.createClass({
       console.log("onEditPath");
   },
   _onCreate: function(e) {
-      
+
       // get geojson from e.
       var polygonJSON = e.layer.toGeoJSON();
-      
+
       var serviceURL = "http://localhost:8088/getVectorData";
-      
-      this.props.queryPolygon(serviceURL, polygonJSON);
-      
-      
-      
+      this.props.queryPolygon(serviceURL, polygonJSON.geometry);
+
+
+
       console.log("onCreate");
   },
   _mounted: function() {
@@ -26,8 +25,8 @@ var Leaflet = React.createClass({
   },
   _onDeleted: function() {
       console.log("deleted");
-  },  
-  
+  },
+
   render: function() {
     const style = {
       minHeight: "800px", width: "100%"
@@ -35,7 +34,7 @@ var Leaflet = React.createClass({
     let tileLayers = _.map(this.props.url, u => {
       return <TileLayer url={u} />;
     });
-    
+
     return (
       <Map center ={[37.062,-121.530]} zoom={8} style={style} bounds={this.props.bounds}>
         <TileLayer
@@ -52,7 +51,7 @@ var Leaflet = React.createClass({
                 rectangle: false
               }}
             />
-        </FeatureGroup>        
+        </FeatureGroup>
         {tileLayers}
       </Map>
     );
