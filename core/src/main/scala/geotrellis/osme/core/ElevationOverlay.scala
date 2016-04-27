42ac565b46dacb253f29ac8e5be58a545795fde2
Arjun Puri <arjunpur@andrew.cmu.edu>
2016-04-27 00:25:31 -0400

    added unit tests and changed overlay sig

--- core/src/main/scala/geotrellis/osme/core/ElevationOverlay.scala
+++ core/src/main/scala/geotrellis/osme/core/ElevationOverlay.scala
@@ -1,8 +1,11 @@
 package geotrellis.osme.core
 
+import java.io.{BufferedWriter, FileWriter, File}
+
 import com.vividsolutions.jts.geom.{LineString, MultiLineString}
 import geotrellis.raster.io.geotiff.SinglebandGeoTiff
 import geotrellis.vector.io.json.{GeoJson, JsonFeatureCollection}
+import scala.collection.immutable.Map
 import spray.json._
 import DefaultJsonProtocol._
 import geotrellis.vector.io.json.FeatureFormats.writeFeatureJson
@@ -10,7 +13,6 @@ import geotrellis.vector.io.json.GeometryFormats._
 import geotrellis.vector.densify.DensifyMethods
 import geotrellis.vector.dissolve.DissolveMethods
 import geotrellis.vector._
-import scala.io.Source
 
 
 /* TODO: Add this to the geotrellis library as a property of Line maybe */
@@ -25,47 +27,34 @@ object Segments {
 
 object ElevationOverlay {
   /* Change to take polygon */
-  def apply(): List[JsValue] = {
-    val gt = SinglebandGeoTiff("data/imgn36w100_13_3_3.tif");
-    val geojson = Source.fromFile("data/imgn36w100vector.geojson").getLines.mkString
-
-    val gjCol = GeoJson.parse[JsonFeatureCollection](geojson)
-    val polygons: Vector[Polygon] = gjCol.getAllPolygons()
-    val lines: Vector[Line] = gjCol.getAllLines()
-    val multiLine = gjCol.getAllLines().toMultiLine
-
+  def apply(geotiff: SinglebandGeoTiff, multiline: MultiLine): Traversable[Feature[Line, Map[String, Double]]] = {
 
     /* TODO: Reproject if necessary */
-    val rasterExtent = gt.rasterExtent
+    val rasterExtent = geotiff.rasterExtent
 
     /* tolerance parameter for densify */
     val densifyParam = rasterExtent.cellwidth
 
-    val densifiedLine = multiLine.densify(densifyParam)
-    val dissolvedLines: MultiLine = densifiedLine.dissolve.asMultiLine.getOrElse(multiLine.jtsGeom)
+    val densifiedLine = multiline.densify(densifyParam)
+    val dissolvedLines: MultiLine = densifiedLine.dissolve.asMultiLine.getOrElse(multiline.jtsGeom)
 
     val numSegments = 5
     val segments = dissolvedLines.lines.map(line => Segments(line, numSegments)).flatten
 
-    val centers = segments.map(line => line.centroid)
-
-    val segmentsJSON = segments.map { segment =>
+    /* For every segment of all lines, map it to the raster and get the corresponding
+     * elevation. Create a LineFeature for this */
+    val segmentsFeatures = segments.map { segment =>
        val center = segment.centroid match {
          case PointResult(p) => p
          case NoResult => throw new Exception("No result found in PointOrNoResult")
        }
        val (col, row) = rasterExtent.mapToGrid(center)
-       val elevation = gt.tile.getDouble(col, row)
-       val meanvMap: Map[String, Double] = Map("MEANV" -> 2.0)
-       writeFeatureJson(LineFeature(segment, meanvMap.toJson)).toJson
+       val elevation = geotiff.tile.getDouble(col, row)
+       val meanvMap: Map[String, Double] = Map("MEANV" -> elevation)
+       LineFeature(segment, meanvMap)
      }
 
-    val file = new File("geocolor_test.json")
-    val bw = new BufferedWriter(new FileWriter(file))
-    bw.write(segmentsJSON)
-    bw.close()
-    //seqGeometryToGeometryCollection(segmentsJ)
-    return segmentsJSON.toList
+    return segmentsFeatures.toTraversable
 
 
   }
